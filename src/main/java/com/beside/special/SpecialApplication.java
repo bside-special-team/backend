1951bd0664e4dd4e541f24a38a2abe24a923fc8e
반선호 <shakevan@woowahan.com>
2022-10-01 21:41:04 +0900

    feat: test Fix

--- src/main/java/com/beside/special/SpecialApplication.java
+++ src/main/java/com/beside/special/SpecialApplication.java
@@ -2,9 +2,7 @@ package com.beside.special;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
-@EnableMongoAuditing
 @SpringBootApplication
 public class SpecialApplication {
     public static void main(String[] args) {

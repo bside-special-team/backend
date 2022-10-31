968ed6956846dc628c2bc35d1ea6d9400c7dcde0
Hwa-ning <hwaning1@douzone.com>
2022-11-01 00:58:34 +0900

    feat: 히든 플레이스 등록 수정 및 방문인증 추가

---
+++ src/main/java/com/beside/special/domain/VisitInfo.java
@@ -0,0 +1,30 @@
+package com.beside.special.domain;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+// User Entity contains Place ID
+// Place Entity contains User ID
+public class VisitInfo{
+    private String id;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @CreatedDate
+    private LocalDateTime visitedAt;
+
+    @Override
+    public boolean equals(Object obj){
+        if(getClass() != obj.getClass()){
+            return false;
+        }
+        return ( ((VisitInfo) obj).id.equals(this.id));
+    }
+}

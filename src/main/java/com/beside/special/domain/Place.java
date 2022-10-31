3f2a9fae6468971aed37d66ea984587476181d1e
Hwa-ning <hwaning1@douzone.com>
2022-11-01 00:57:38 +0900

    feat: 플레이스 불필요 정보 수정

--- src/main/java/com/beside/special/domain/Place.java
+++ src/main/java/com/beside/special/domain/Place.java
@@ -2,6 +2,7 @@ package com.beside.special.domain;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
+import lombok.Setter;
 import org.apache.logging.log4j.util.Strings;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -10,58 +11,46 @@ import org.springframework.util.CollectionUtils;
 import java.time.LocalTime;
 import java.util.List;
 import java.util.Objects;
+import java.util.Set;
 
 @Getter
+@Setter
 @Document
 public class Place extends BaseEntity {
     private static final int MAX_NAME_LENGTH = 10;
     @Id
     private String id;
 
-    private PlaceType placeType;
+    private User writer;
 
-    private Category category;
+    private PlaceType placeType;
 
     private Coordinate coordinate;
 
     private String name;
 
-    private String description;
-
     private List<String> imageUrls;
 
-    private Integer visitCount;
-
-    @JsonFormat(pattern = "HH:mm")
-    private LocalTime bestStartTime;
-
-    @JsonFormat(pattern = "HH:mm")
-    private LocalTime bestEndTime;
+    private Integer visitCount = 1;
 
     private List<String> hashTags;
 
-    private Season season;
+    private Set<VisitInfo> visitInfos;
 
-    public Place(Category category, Coordinate coordinate, String name, String description, Integer visitCount,
-                 List<String> imageUrls, LocalTime bestStartTime, LocalTime bestEndTime, List<String> hashTags,
-                 Season season) {
-        validate(category, coordinate, name, hashTags);
-        //TODO 사진 validation
+    public Place(Coordinate coordinate, String name,  User writer,
+                 List<String> imageUrls, List<String> hashTags) {
+        // validate(coordinate, name, hashTags);
+        // TODO 사진 validation
         this.placeType = PlaceType.HIDDEN;
-        this.category = category;
         this.coordinate = coordinate;
         this.name = name;
-        this.description = description;
+        this.writer = writer;
         this.imageUrls = imageUrls;
-        this.visitCount = visitCount;
-        this.bestStartTime = bestStartTime;
-        this.bestEndTime = bestEndTime;
         this.hashTags = hashTags;
-        this.season = season;
     }
 
     private static void validate(Category category, Coordinate coordinate, String name, List<String> hashTags) {
-        Objects.requireNonNull(category);
+        // Objects.requireNonNull(category);
         Objects.requireNonNull(coordinate);
         Objects.requireNonNull(name);
 

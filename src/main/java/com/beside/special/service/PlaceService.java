13a8c5abf61bee7002de299a98d11c033fa742e5
Hwa-ning <hwaning1@douzone.com>
2022-11-01 00:58:20 +0900

    feat: 히든 플레이스 등록 수정 및 방문인증 추가

--- src/main/java/com/beside/special/service/PlaceService.java
+++ src/main/java/com/beside/special/service/PlaceService.java
@@ -1,41 +1,71 @@
 package com.beside.special.service;
 
-import com.beside.special.domain.Category;
-import com.beside.special.domain.Place;
-import com.beside.special.domain.PlaceRepository;
+import com.beside.special.domain.*;
 import com.beside.special.service.dto.CreatePlaceDto;
+import com.beside.special.service.dto.VisitPlaceDto;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.ArrayList;
 
 @Service
 public class PlaceService {
     private final PlaceRepository placeRepository;
-    private final CategoryService categoryService;
+    private final UserRepository userRepository;
 
-    public PlaceService(PlaceRepository placeRepository, CategoryService categoryService) {
+    public PlaceService(PlaceRepository placeRepository, UserRepository userRepository) {
         this.placeRepository = placeRepository;
-        this.categoryService = categoryService;
+        this.userRepository = userRepository;
     }
 
     @Transactional
     public Place create(CreatePlaceDto createPlaceDto) {
-        Category category = categoryService.findByCode(createPlaceDto.getCategoryCode());
-        //TODO Image update & Url 리스트 받아오기
+        User writer = userRepository.findById(createPlaceDto.getUserId())
+                .orElseThrow(() -> new IllegalArgumentException("존재하지않는 User"));
 
-        Place place = new Place(category,
-            createPlaceDto.getCoordinate(),
-            createPlaceDto.getName(),
-            createPlaceDto.getDescription(),
-            createPlaceDto.getVisitCount(),
-            new ArrayList<>(),
-            createPlaceDto.getBestStartTime(),
-            createPlaceDto.getBestEndTime(),
-            createPlaceDto.getHashTags(),
-            createPlaceDto.getSeason()
-            );
+        Place place = new Place(
+                createPlaceDto.getCoordinate(),
+                createPlaceDto.getName(),
+                writer,
+                createPlaceDto.getImages(),
+                createPlaceDto.getHashTags()
+        );
+
+        placeRepository.save(place);
 
         return placeRepository.save(place);
+    }
+
+    @Transactional
+    public Place visit(VisitPlaceDto visitPlaceDto) {
+        Place place = placeRepository.findById(visitPlaceDto.getPlaceId())
+                .orElseThrow(() -> new IllegalArgumentException("존재하지않는 Place"));
+
+        User user = userRepository.findById(visitPlaceDto.getUserId())
+                .orElseThrow(() -> new IllegalArgumentException("존재하지않는 User"));
+
+        VisitInfo placeVisitInfo = VisitInfo.builder()
+                .id(user.getId())
+                .build();
+
+        System.out.println(placeVisitInfo.getId() + " / " + placeVisitInfo.getVisitedAt());
+
+        VisitInfo userVisitInfo = VisitInfo.builder()
+                .id(place.getId())
+                .build();
+
+        System.out.println(userVisitInfo.getId() + " / " + userVisitInfo.getVisitedAt());
+
+        if (user.getVisitInfos().contains(userVisitInfo) || place.getVisitInfos().contains(placeVisitInfo) ){
+            throw new IllegalArgumentException("이미 방문한 장소입니다.");
+        }
+
+        user.getVisitInfos().add(userVisitInfo);
+        place.getVisitInfos().add(placeVisitInfo);
+
+        place.setVisitCount(place.getVisitCount() + 1);
+
+        placeRepository.save(place);
+        userRepository.save(user);
+
+        return place;
     }
 }

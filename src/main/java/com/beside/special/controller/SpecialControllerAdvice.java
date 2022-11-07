a0b619f0816aa3bf7518375de4ff1c97cb8786bf
반선호 <vsh123@naver.com>
2022-11-07 23:32:17 +0900

    feat: exception 발생시 httpStatus + body에 메시지 포함

--- src/main/java/com/beside/special/controller/SpecialControllerAdvice.java
+++ src/main/java/com/beside/special/controller/SpecialControllerAdvice.java
@@ -1,16 +1,19 @@
 package com.beside.special.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.stream.Collectors;
 
+@Slf4j
 @RestControllerAdvice
 public class SpecialControllerAdvice extends ResponseEntityExceptionHandler {
     @Override
@@ -21,5 +24,12 @@ public class SpecialControllerAdvice extends ResponseEntityExceptionHandler {
             .map(DefaultMessageSourceResolvable::getDefaultMessage)
             .collect(Collectors.joining("\n"));
         return ResponseEntity.badRequest().body(message);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception exception) {
+        log.error("unknown exception throws {}", exception.getMessage(), exception);
+
+        return ResponseEntity.internalServerError().body(exception.getMessage());
     }
 }

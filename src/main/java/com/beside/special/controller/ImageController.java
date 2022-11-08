d4dfe2cb73cc5451d6e3e490f55d2dd2d8c7d875
Hwa-ning <hwaning1@douzone.com>
2022-11-08 22:54:26 +0900

    feat: 이미지 업로드 로직 개선 및 이미지 다운로드 추가 (선호님 리뷰적용)

--- src/main/java/com/beside/special/controller/ImageController.java
+++ src/main/java/com/beside/special/controller/ImageController.java
@@ -1,25 +1,25 @@
 package com.beside.special.controller;
 
-import com.beside.special.service.ImageUploader;
+import com.beside.special.service.ImageService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
 import java.util.List;
 
 @Tag(name = "Image", description = "이미지 처리 관련")
 @RequestMapping("/api/v1/images")
 @RestController
 public class ImageController {
-    private final ImageUploader imageUploader;
+    private final ImageService imageService;
 
-    public ImageController(ImageUploader imageUploader) {
-        this.imageUploader = imageUploader;
+    public ImageController(ImageService imageService) {
+        this.imageService = imageService;
     }
 
     @Operation(summary = "이미지 등록 (multipart/form-data)", responses = {
@@ -27,23 +27,22 @@ public class ImageController {
             @ApiResponse(responseCode = "500", description = "서버 에러")
     })
     @PostMapping
-    public ResponseEntity uploadImage(
+    public ResponseEntity<List<String> > uploadImage(
             @RequestParam("images") List<MultipartFile> images,
-            @RequestParam("dest") String dest) throws IOException {
+            @RequestParam("dest") String dest){
         return ResponseEntity.status(HttpStatus.CREATED)
-                .body(imageUploader.uploadImage(images, dest));
+                .body(imageService.uploadImage(images, dest));
     }
 
-    // TODO WEBP 변환 및 이미지 최적화
-    @Operation(summary = "미구현 / 이미지 최적화 관련", responses = {
-            @ApiResponse(responseCode = "201", description = "이미지 등록 완료"),
+    // TODO WEBP 변환
+    @Operation(summary = "이미지 다운로드", responses = {
+            @ApiResponse(responseCode = "200", description = "이미지 조회 성공 및 다운로드"),
             @ApiResponse(responseCode = "500", description = "서버 에러")
     })
-    @GetMapping("/{imageUrl}")
-    public byte[] resizeImage(@PathVariable String imageUrl){
-        return null;
-//        return ResponseEntity.ok()
-//                .contentType(MediaType.IMAGE_JPEG)
-//                .body("image");
+    @GetMapping("/{imageUUID}")
+    public ResponseEntity<byte[]> resizeImage(@PathVariable String imageUUID) {
+        return ResponseEntity.status(HttpStatus.OK)
+                .contentType(MediaType.IMAGE_JPEG)
+                .body(imageService.getImage(imageUUID).toByteArray());
     }
 }
---
+++ src/main/java/com/beside/special/service/ImageService.java
@@ -0,0 +1,120 @@
+package com.beside.special.service;
+
+import com.amazonaws.SdkClientException;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.*;
+import com.beside.special.domain.Image;
+import com.beside.special.domain.ImageRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class ImageService {
+    private static final String BUCKET_NAME = "team.special";
+
+    private final AmazonS3 s3;
+    private final ImageRepository imageRepository;
+
+    @Autowired
+    public ImageService(ImageRepository imageRepository, AmazonS3 s3) {
+        this.imageRepository = imageRepository;
+        this.s3 = s3;
+    }
+    // 이미지 업로드 ( MultiPartFile, 저장 디렉토리 ) TODO File Size 처리 (제한?)
+    public List<String> uploadImage(List<MultipartFile> images, String targetDirectory) {
+        List<String> imageUuids = new ArrayList<>();
+
+        for (MultipartFile file : images) {
+            validateImage(file);
+
+            String fileKey = targetDirectory + "/" + getImageKey(file);
+            try {
+                s3.putObject(new PutObjectRequest(
+                        BUCKET_NAME,
+                        fileKey,
+                        file.getInputStream(),
+                        new ObjectMetadata()));
+
+            } catch (SdkClientException | IOException e) {
+                throw new SdkClientException("S3 Upload 중 오류 발생");
+            }
+
+            Image image = Image.builder()
+                    .fileKey(fileKey)
+                    .uuid(String.valueOf(UUID.randomUUID()))
+                    .build();
+
+            imageRepository.save(image);
+
+            imageUuids.add(image.getUuid());
+        }
+        return imageUuids;
+    }
+
+    public ByteArrayOutputStream getImage(String uuid) {
+        Image image = imageRepository.findByUuid(uuid)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 파일입니다."));
+
+        S3Object s3Object = s3.getObject(BUCKET_NAME, image.getFileKey());
+
+        try (InputStream is = s3Object.getObjectContent();
+             ByteArrayOutputStream outputStream = new ByteArrayOutputStream()) {
+
+            int read_len = 0;
+            byte[] read_buffer = new byte[4096];
+            while ((read_len = is.read(read_buffer)) > 0) {
+                outputStream.write(read_buffer, 0, read_len);
+            }
+            return outputStream;
+        } catch (IOException e) {
+            throw new IllegalArgumentException("존재하지 않는 파일입니다.");
+        }
+    }
+
+    public void deleteImage(List<String> uuids) {
+        for (String uuid : uuids) {
+            Image image = imageRepository.findByUuid(uuid)
+                    .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 파일입니다."));
+
+            s3.deleteObject(BUCKET_NAME, image.getFileKey());
+        }
+    }
+
+    // MultiFile Image 검증
+    private void validateImage(MultipartFile multipartFile) {
+        if (multipartFile.isEmpty()) {
+            throw new IllegalArgumentException("빈 파일입니다.");
+        }
+        String[] tokens = multipartFile.getContentType().split("/");
+        if (!tokens[0].equals("image")) {
+            throw new IllegalArgumentException("잘못된 파일 타입입니다.");
+        }
+    }
+
+    // Image가 저장될 Key 생성
+    private String getImageKey(MultipartFile file) {
+        return UUID.randomUUID() + "-" + file.getOriginalFilename();
+    }
+
+    // 버킷 존재 유무 확인 (버킷명)
+    private boolean bucketIsExist(String targetBucket) {
+        try {
+            List<Bucket> buckets = s3.listBuckets();
+            for (Bucket bucket : buckets) {
+                if (targetBucket.equals(bucket.getName())) {
+                    return true;
+                }
+            }
+        } catch (SdkClientException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+}

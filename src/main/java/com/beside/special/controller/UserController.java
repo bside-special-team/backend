d9cebe115f3f36f166b0c4a6afc54e1da2faf9fa
반선호 <vsh123@naver.com>
2022-11-20 02:35:51 +0900

    feat: 회원정보 조회 시 출석 여부 체크

--- src/main/java/com/beside/special/controller/UserController.java
+++ src/main/java/com/beside/special/controller/UserController.java
@@ -9,6 +9,7 @@ import com.beside.special.domain.dto.UserUpdateRequest;
 import com.beside.special.service.AccessTokenService;
 import com.beside.special.service.RefreshTokenService;
 import com.beside.special.service.UserService;
+import com.beside.special.service.dto.GainPointResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -36,8 +37,8 @@ public class UserController {
     }
 
     @GetMapping("one")
-    public User getUser(@Parameter(hidden = true) @AuthUser UserDto userDto) {
-        return userService.findById(userDto.getUserId());
+    public GainPointResponse<User> getUser(@Parameter(hidden = true) @AuthUser UserDto userDto) {
+        return userService.findByIdWithAttendance(userDto.getUserId());
     }
 
     @Operation(summary = "닉네임 업데이트", responses = {
--- src/main/java/com/beside/special/domain/UserPointHistoryRepository.java
+++ src/main/java/com/beside/special/domain/UserPointHistoryRepository.java
@@ -3,4 +3,5 @@ package com.beside.special.domain;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface UserPointHistoryRepository extends MongoRepository<UserPointHistory, String> {
+    boolean existsByUser_IdAndTargetIdAndPointAction(String userId, String targetId, PointAction action);
 }
